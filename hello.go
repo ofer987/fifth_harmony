0c9dd5cda3af600e7b75a3643259d762ce47c539
Dan Jakob Ofer <ofer987@gmail.com>
2015-07-03 15:42:05 -0400

    it does a lot more now. thanks to Maryan.

--- hello.go
+++ hello.go
@@ -1,13 +1,25 @@
 package main
 
 import "fmt"
+import "io/ioutil"
+import "io"
 
 import "net/http"
+
+type Response struct {
+	Body io.ReadCloser
+}
+
+func (resp Response) String() string {
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	return string(body)
+}
 
 func main() {
 	fmt.Println("hello world")
 
-	sendHttpGet("www.cnet.com")
+	sendHttpGet("http://www.cnet.com")
 	// var givenName string
 	// givenName = askMeMyName()
 	//
@@ -23,8 +35,21 @@ func askMeMyName() string {
 }
 
 func sendHttpGet(url string) {
-	var response http.Response
-	var err error
-
+	// var response http.Response
+	// var err error
+	//
 	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if response.StatusCode == 200 {
+		// body, _ := ioutil.ReadAll(response.Body)
+		body := Response{response.Body}
+
+		fmt.Printf("%s", body)
+	} else {
+		fmt.Printf("The Status code is %s", response.StatusCode)
+	}
 }
